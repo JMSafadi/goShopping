ae6c4a0f3a982bab213ee9a90437898804050be5
Julian Safadi <julianmatiassafadi@hotmail.com>
2023-08-02 15:07:22 -0300

    [build] layout

---
+++ components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
+    <div class="max-w-[1200px] mx-auto px-4">
+      <div class="md:flex gap-4 justify-between w-full">
+
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">Help</div>
+          <div class="text-[#999999] text-sm">Help Center, Disputes & Resports, Buyer PRotection, REport IPR itnrint, Regular</div>
+        </div>
+        
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">HoShopping Multi-Language Sites</div>
+          <div class="text-[#999999] text-sm">Russian, Portuguese, Spanish, French, Germain, Italian, Dutch, Turkish, Japan,</div>
+        </div>
+
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">Browse by Category</div>
+          <div class="text-[#999999] text-sm">All Popular, Product, PRomotion, Low Price, Greate VAlue, Reviews, Blog, Seller</div>
+        </div>
+        
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">Alibaba Group</div>
+          <div class="text-[#999999] text-sm">Alibaba Group Website, AliExpress, Alimama, Alipay, Fliggy, Alibaba Cloud, Alibaba</div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <div class="w-full bg-[#333333] text-sm text-gray-400">
+    <div class="max-w-[1200px] mx-auto p-4">
+      Intellectual PRoperty Protection - Privacy Policy - Sitemap - Terms of Use
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
